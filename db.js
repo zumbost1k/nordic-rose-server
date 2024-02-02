d1741c56a35b7174a34a9795839f4610d6b1affe
zumbost1k <matusevich.mi@gmail.com>
2024-02-02 10:50:00 +0300

    add pg

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require('sequelize');
+const pg = require('pg');
 
 module.exports = new Sequelize(
  process.env.POSTGRES_URL, // Connection string from Vercel
  {
     dialect: 'postgres',
+    dialectModule: pg,
     dialectOptions: {
       ssl: {
         rejectUnauthorized: false, // Required for Heroku
